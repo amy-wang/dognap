7e061076967bc02621889c0ef31f2b5ec524d7a5
Yucen Zhang <yucen.zhang0205@gmail.com>
2017-05-27 19:53:31 -0400

    link setting to story board

--- Dog Nap Alarm/AlarmViewController.swift
+++ Dog Nap Alarm/AlarmViewController.swift
@@ -1,31 +1,30 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Dog Nap Alarm
 //
-//  Created by Amy Wang on 2017-05-27.
+//  Created by Yucen Zhang on 2017-05-27.
 //  Copyright © 2017 Rain Pillar. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class AlarmViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func chooseDog(){
-        let storyboard = UIStoryboard(name: "alarm", bundle: nil)
-        let controller = storyboard.instantiateViewController(withIdentifier: "alarm")
+
+    @IBAction func settingsPage(){
+        let storyboard = UIStoryboard(name: "settings", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "settings")
         self.present(controller, animated: true, completion: nil)
     }
 
     
 }
-
