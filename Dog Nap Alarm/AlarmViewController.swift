3f2862c6538e7bda5ca5e9250cab98207785ec23
Yucen Zhang <yucen.zhang0205@gmail.com>
2017-05-27 22:24:34 -0400

    alarm update

--- Dog Nap Alarm/AlarmViewController.swift
+++ Dog Nap Alarm/AlarmViewController.swift
@@ -10,6 +10,75 @@ import UIKit
 
 class AlarmViewController: UIViewController {
     
+    var seconds = 60
+    var timer = Timer()
+    var resumeTapped = false
+    var isTimerunning = false
+    
+    
+    // method for counting down
+    func runtimer(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(AlarmViewController.updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBOutlet weak var sliderOutlet: UISlider!
+    
+    @IBAction func timeSlider(_ sender: UISlider) {
+        seconds = Int(sender.value)
+        timerLabel.text = String(seconds)
+    }
+   
+    @IBOutlet weak var startOutlet: UIButton!
+    // Start Button function
+    @IBAction func startBtn(_ sender: UIButton) {
+        runtimer()
+        startOutlet.isHidden = true
+        sliderOutlet.isHidden = true
+        stopOutlet.isHidden = false
+        cancelOutlet.isHidden = false
+    }
+    
+    // function to update timer
+    func updateTimer(){
+        seconds -= 1 //this will decrement the time
+        timerLabel.text = String(seconds)
+        
+        if(seconds == 0){
+            timer.invalidate()
+        }
+    }
+    
+    
+    @IBOutlet weak var stopOutlet: UIButton!
+    
+    // Stop Button function
+    @IBAction func stopBtn(_ sender: UIButton) {
+        if self.resumeTapped == false {
+            timer.invalidate()
+            self.resumeTapped = true
+        } else {
+            runtimer()
+            self.resumeTapped = false
+        }
+    }
+    
+    
+    @IBOutlet weak var cancelOutlet: UIButton!
+    @IBAction func cancelBtn(_ sender: UIButton) {
+        timer.invalidate()
+        seconds = 30
+        sliderOutlet.setValue(30, animated: true)
+        timerLabel.text = "30"
+        startOutlet.isHidden = false
+        sliderOutlet.isHidden = false
+        stopOutlet.isHidden = true
+        cancelOutlet.isHidden = true
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
