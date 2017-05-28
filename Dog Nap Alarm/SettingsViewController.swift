70852f4ab05fa3b44382c0211a13abac0839915d
Amy Wang <amy.h.wang@hotmail.com>
2017-05-27 20:31:44 -0400

    function for back button

---
+++ Dog Nap Alarm/SettingsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsViewController.swift
+//  Dog Nap Alarm
+//
+//  Created by Amy Wang on 2017-05-27.
+//  Copyright © 2017 Rain Pillar. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backToAlarm(){
+        let storyboard = UIStoryboard(name: "alarm", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "alarm")
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+}
