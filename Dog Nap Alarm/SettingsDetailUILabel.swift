e6fa43f809e321689556fda81e9dc6ac7d00a519
Amy Wang <amy.h.wang@hotmail.com>
2017-05-30 17:17:53 -0400

    changed stored setting value

---
+++ Dog Nap Alarm/SettingsDetailUILabel.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsDetailUILabel.swift
+//  Dog Nap Alarm
+//
+//  Created by Amy Wang on 2017-05-30.
+//  Copyright © 2017 Rain Pillar. All rights reserved.
+//
+
+import UIKit
+
+class SettingsDetailUILabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
